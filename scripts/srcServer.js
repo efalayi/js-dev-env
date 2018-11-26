0bef613fa4c5768903d7c55366f01b4d958d2bcb
Esther Falayi <esther.falayi@andela.com>
2018-11-26 22:14:07 +0100

    chore(transpile): configure babel
    - re-install babel dependencies
    - use es6 import statements

--- scripts/srcServer.js
+++ scripts/srcServer.js
@@ -1,16 +1,16 @@
-var express = require('express')
-var path = require('path')
-var open = require('open')
+import express from 'express'
+import path from 'path'
+import open from 'open'
 
-var PORT =  3000
-var app = express()
+const PORT =  3000
+const app = express()
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   console.log('app running on port: ', PORT)
   res.sendFile(path.join(__dirname, '../src/index.html'))
 })
 
-app.listen(PORT, function(error) {
+app.listen(PORT, (error) => {
   if (error) {
     console.log(error);
   } else {
